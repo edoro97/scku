b475cdc7c55f0c8bbf2183df80cd525f5d87b5e8
edoro97 <110031019+edoro97@users.noreply.github.com>
2022-10-21 18:03:19 +0700

    Add files via upload

---
+++ plugins/Kaptensparrow.js
@@ -0,0 +1,125 @@
+let handler = async m => m.reply(`
+            *🏅 BONUS DEPO 100% ALL MEMBER 🏅*
+
+*GRUP CLAIM WA👇*
+_https://chat.whatsapp.com/L0QItQ9Q5ORLWTEVSnuo1j_
+
+*GRUP CLAIM TELE👇*
+_https://t.me/+64h2SQR1UrNjMWE1_
+
+
+*PROSES CLAIM BONUS👇*
+▪️ SS DAFTAR & SS DEPO MASUK AKUN
+▪️ BONUS MASUK DULU BARU DIMAINKAN
+
+
+
+*🎰 SUHU88 BIG BONUS*
+_http://bit.ly/BIGBONUS_SUHU88_
+100K + 100K TO X7
+300K + 300K TO X7
+500K + 500K TO X7
+1JT + 1JT TO X7
+MAX BONUS 2JT
+
+*DILARANG SAMA DATA REK*
+
+
+
+*🎰 SUHU88*
+_http://bit.ly/Link_Suhu88_
+NEW
+30K + 15K TO X3
+50K + 25K TO X3
+
+OLD
+100K + 50K TO X5
+200K + 100K TO X5
+
+*BEBAS DATA REK*
+*OLD BEDA REF JUGA BISA CLAIM*
+
+
+
+*🎰 TURBO188*
+_http://bit.ly/Link_Turbo188_
+NEW
+30K + 15K TO X1
+
+OLD
+50K + 15K TO X2
+100K + 25K TO X2
+
+*DILARANG SAMA DATA REK*
+*OLD BEDA REF JUGA BISA CLAIM*
+
+
+
+*🎰 ARENA899*
+_http://bit.ly/Link_Arena899_
+20K + 10K TO X3
+30K + 15K TO X3
+50K + 25K TO X5
+
+*1 NAMA REK MAX 2 ID*
+
+
+
+*🎰 KOMPAS138*
+_http://bitly.ws/vucZ_
+25K + 25K WO X5
+50K + 50K WO X5
+100K + 100K WO X5
+
+*EWALLET HANYA BISA DANA*
+*DILARANG SAMA DATA REK*
+
+
+
+*🎰 OWL77*
+_https://rebrand.ly/642nj10_
+20K + 20K TO X3
+MAX WD 300K
+
+*BEBAS DATA REK*
+*DILARANG BUY SPIN*
+
+
+
+*🎰 ALE999*
+_http://bit.ly/Link_Ale999_
+50K + 50K TO X5
+MAX WD 500K
+100K + 100K TO X5
+MAX WD 1JT
+
+*DILARANG BUY SPIN*
+*DILARANG SAMA DATA REK*
+*PRAGMATIC ONLY*
+
+
+
+*🎰 MESINUANG88*
+_https://bit.ly/3BvzCFX_
+20K + 20K TO X7
+30K + 20K TO X7
+50K + 30K TO X7
+
+*DILARANG SAMA DATA REK*
+
+
+
+▪️ BEBAS IP,KECUALI KOMPAS
+▪️ ALL SLOT,KECUALI ALE
+▪️ ALL BANK & EWALLET, KECUALI KOMPAS,DANA
+▪️ BEBAS BUY SPIN,KECUALI ALE & OWL
+▪️ DILARANG SPACEMAN & CLASSIC SLOT
+
+`.trim()) //
+
+handler.customPrefix = /^(.kaptensparrow|kaptensparrow)$/i
+handler.command = new RegExp
+
+handler.limit = false
+handler.mods = false 
+module.exports = handler
